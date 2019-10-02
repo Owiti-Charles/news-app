6638bb9ba3c2d1d310d120490f10d056dfd9d4a1
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-10-02 11:37:57 +0300

    Add search button to allow a user to search for news articles

--- news/views.py
+++ news/views.py
@@ -29,4 +29,13 @@ def past_day_news(request, past_date):
 
 
 def search_results(request):
-    pass
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Articles.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-news/search.html', {"message": message, "articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-news/search.html', {"message": message})
